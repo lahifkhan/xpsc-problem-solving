b03826582694f1d0af31da2cc9332ade8d37eaae
lahifkhan <121628379+lahifkhan@users.noreply.github.com>
2024-11-14 23:12:17 +0600

    Add files via upload

---
+++ weak-3/day 5/slidingWindow.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long maximumSumSubarray(vector<int>& arr, int k) {
+        // code here
+        int n= arr.size();
+        int l=0, r=0;
+        long long int sum=0;
+        long long int ans= 0;
+        while(r<n){
+            sum = sum+arr[r];
+            if((r-l)+1 == k){
+                ans = max(sum,ans);
+                sum = sum-arr[l];
+                l++;
+                r++;
+                
+            }
+            else{
+                r++;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution ob;
+        long long ans = ob.maximumSumSubarray(arr, k);
+        cout << ans << "\n";
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
