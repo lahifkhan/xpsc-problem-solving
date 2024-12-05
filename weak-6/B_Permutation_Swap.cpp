59765f267f3a2eb95621e6783df70c7042fb4739
lahifkhan <121628379+lahifkhan@users.noreply.github.com>
2024-12-05 22:38:00 +0600

    Add files via upload

---
+++ weak-6/B_Permutation_Swap.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int t;
+    cin>> t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> v(n+1);
+        for(int i=1; i<=n; i++){
+            cin>> v[i];
+        }
+
+        int ans =0;
+        for(int i=1; i<=n; i++){
+              ans=__gcd(ans,abs(v[i]-i));
+        }
+        cout << ans <<"\n";
+    }
+    return 0;
+}
