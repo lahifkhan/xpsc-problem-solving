3f1f79e39aa3fd54f7341ba9d30c99c27a68ff12
lahifkhan <121628379+lahifkhan@users.noreply.github.com>
2024-12-09 23:08:45 +0600

    Add files via upload

---
+++ weak-7/day 2/B_Fun_Game.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   
+
+   int q;
+   cin >> q;
+   while (q--) {
+      int n;
+      cin >> n;
+      string s, t;
+      cin >> s >> t;
+      if (s == t) {
+         cout << "YES" << '\n';
+         continue;
+      }
+
+      int pos = -1;
+      for (int i = 0;i < n;i++) {
+         if (s[i] == '1') {
+            pos = i;
+            break;
+         }
+      }
+
+      if (pos != -1) {
+         bool ok = true;
+         for (int i = 0;i < n;i++) {
+            if (s[i] != t[i]) {
+               if (pos > i) {
+                  ok = false;
+                  break;
+               }
+            }
+         }
+         if (ok) {
+            cout << "YES" << '\n';
+         }
+         else {
+            cout << "NO" << '\n';
+         }
+      }
+      else {
+         cout << "NO" << '\n';
+      }
+   }
+   return 0;
+}
